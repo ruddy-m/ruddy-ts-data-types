95760bff4c32e83223e4eca6246d344e1010cce1
Ruddy <ruddy.riba@umuzi.org>
2023-07-01 10:06:07 +0200

    Add data type project

---
+++ src/main/java/DataTypes.java
@@ -0,0 +1,53 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+        // Boolean
+        Boolean firstBool = 1;
+        boolean secondBool = false;
+        Boolean thirdBool = true;
+
+        // Bytes 8-bit
+        byte firstByte = 300;
+        Byte secondByte = 10000;
+        byte thirdByte = -2;
+
+        // Short 16-bit
+        Short firstShort = -1;
+        short secondShort = -33000;
+
+        // Integer 32bit
+        Integer firstInt =  new Integer(1);
+        int newFirstInt =  1;
+        int secondInt = new Interger(23);
+        int thirdInt = 3000000000;
+
+       // Long 64-bit
+        long firstLong = 2;
+        Long secondLong = 3000000000;
+        Long thirdLong = 3000000000L;
+        Long fourthFloat = 33f;
+
+        // Float
+        float firstFloat = 32; // int
+        float secondFloat = 34f;
+        Float thirdFloat = 3.4F;
+        float sixFloat = 3.4;
+        float fifthFloat = 3000000000L; //long
+
+        System.out.println("firstFloat: " + firstFloat);
+
+        // Double
+        Double firstDouble = 2.3.4;
+        double secondDouble = 23.9f;
+        Double thirdDecimal = 2.2;
+        double fourthDecimal = 23;
+
+        // Char
+        char firstChar = "word";
+        Char secondChar = "\u0000";
+        char thirstChar = '$';
+        char fourthChar = "w";
+        char fifthChar = 'A' + 1;
+
+    }
+}
